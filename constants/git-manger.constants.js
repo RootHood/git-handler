13910b06e9cf9ceaac1a9a2afa9ee37a56108c76
carlos <carlosramos@sime.es>
2022-03-26 21:24:50 +0100

    change pull condition + 2

--- constants/git-manger.constants.js
+++ constants/git-manger.constants.js
@@ -3,4 +3,4 @@ export const STORAGE_PATH = './repos.json';
 
 // GIT
 export const NO_COMMITS = 'nothing to commit, working tree clean';
-export const PULL = 'use "git pull" to merge the remote branch into yours';
+export const PULL = 'Your branch and \'origin/develop\' have diverged';
